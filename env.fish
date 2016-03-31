3c74059c6d92102131858646a664a3694edc2439
eugenesvk <eugenesvk@gmail.com>
2016-03-31 20:57:52 +0300

    Replace `which` with `command` to avoid errors. Adjust environment to include ssh agent support

--- env.fish
+++ env.fish
@@ -6,6 +6,7 @@ set -x LANG        	en_US.UTF-8
 set -x ARCHFLAGS   	"-arch x86_64"	# Compilation flags
 
 # In `fish` aliases are left for compatibility, defined as functions; `functions` for a full list
+alias pkg-size "pacman -Qi | egrep '^(Name|Installed)' | cut -f2 -d':' | paste - - | column -t | sort -nrk 2 | grep MiB | less"
 alias py      "python"
 alias cls     "clear"                        	# Clear screen
 alias del     "rmtrash"                      	# Remove to trash
@@ -26,7 +27,6 @@ if test "$system" = OSX
 		#additional command ${DYLD_INSERT_LIBRARIES:+:$DYLD_INSERT_LIBRARIES} in the end, but seems like a duplicate
 end
 
-
 # PATH
 set -x 	EDITOR        	"subl -w"
 set -x 	DevH          	$HOME/.dev  	#local development environment for virutalenvs etc.
@@ -39,11 +39,9 @@ set -gx	NVM_DIR       	$DevH/.nvm  	#local NVM versions/shims; default ~/.nvm
 set -x 	PLENV_ROOT    	$DevH/.plenv	#local Per versions/shims; default ~/.plenv
 set -x 	CoreUtils_Root	/usr/local/opt/coreutils/libexec
 set -x 	MANPATH       	$CoreUtils_Root/gnuman /usr/local/share/man $MANPATH
-set -x 	SSH_KEY_PATH  	$HOME/.ssh/rsa_id
-set -x 	PATH          	$CoreUtils_Root/gnubin $HOME/bin $HOME/bin/FontForge\ Tools /usr/local/sbin $PATH
+set -x 	SSH_KEY_PATH  	$HOME/.ssh/IDs/rsa_id
+set -x 	PATH          	$CoreUtils_Root/gnubin $HOME/bin $HOME/bin/FontForgeTools /usr/local/sbin $PATH
 
-#set -x PATH $PYENV_ROOT/bin $HOME/.rbenv/bin $HOME/.plenv/bin $PATH  # not needed since binaries are in Homebrew
-#$PYENV_ROOT/shims $HOME/.rbenv/shims $HOME/.plenv/shims $PATH      #get installed through init commands above
 #/usr/local/bin /usr/bin /bin /usr/sbin /sbin #default in '/private/etc/paths'
 #/opt/X11/bin /usr/local/MacGPG2/bin          #manually added in '/private/etc/paths.d'
 
@@ -51,12 +49,38 @@ set -x 	PATH          	$CoreUtils_Root/gnubin $HOME/bin $HOME/bin/FontForge\ Too
 set -x HOMEBREW_GITHUB_API_TOKEN    	(cat $HOME/.config/env_github_brew.txt)
 set -x ST3_MARKDOWN_GITHUB_API_TOKEN	(cat $HOME/.config/env_github_ST3.txt)
 
-# Search for compatible SSH agents and set `SSH_AUTH_SOCK` accordingly
-# ssh-find-agent
-# eval (ssh-agent) 1>/dev/null     # launch ssh-agent unless already exists
-# . $HOME/.ssh/ssh-find-agent.fish
-# ssh-find-agent -a
-# if [ -z "$SSH_AUTH_SOCK" ]
-#    eval $(ssh_agent) 2>/dev/null
-#    ssh-add -l 1>/dev/null; or alias ssh='ssh-add -l 1>/dev/null; or ssh-add; and unalias ssh; ssh'
-# end
+# Set vars that would allow ssh-add -c confirmation option to work
+set -gx DISPLAY "localhost:0.0"
+set -gx SSH_ASKPASS "/usr/lib/git-core/git-gui--askpass"
+
+source $HOME/.config/ssh-find-agent.fish	# find all ssh-agents
+#echo "====Before using `ssh-find-agent -a` (=set_ssh_agent_socket)"; ssh-env-echo
+ssh-find-agent -a	# choose the first agent and set SSH_AUTH_SOCK; `-c` manually
+#echo "====After using `ssh-find-agent -a`"; ssh-env-echo
+
+if test -z "$SSH_AUTH_SOCK"           # if no agent socket set, run new
+	#echo "====New agent since SSH_AUTH_SOCK was empty"; ssh-env-echo
+	#ssh-agent-fish
+	# if no keys, make next `ssh` command start an agent, add all keys and continue as normal
+	ssh-add -l >/dev/null 2>/dev/null; or alias ssh 'ssh-agent-fish; and ssh-add -l >/dev/null 2>/dev/null; or ssh-add -c $HOME/.ssh/IDs/*.pri; and functions -e ssh; ssh'
+	#-c Added identities should be subject to confirmation before being used for authentication
+	#-l Lists fingerprints of all identities currently represented by the agent
+end
+
+#ssh-agent -k; set -e SSH_AGENT_PID; set -e SSH_AUTH_SOCK
+#set -Ux SSH_AGENT_PID 1234; set -Ux SSH_AUTH_SOCK /tmp; echo "Env.fish:AgentPID:[$SSH_AGENT_PID]; Socket[$SSH_AUTH_SOCK]"
+#set -Ux SSH_AUTH_SOCK /tmp; set -e UNITEMP; set -Ux UNITEMP abc;  echo "Env.fish:Socket[$SSH_AUTH_SOCK]"
+#set -Ux SSH_AUTH_SOCK /tmp; set -e UNITEMP1; set -Ux UNITEMP1 abc;  echo "Env.fish:Socket[$SSH_AUTH_SOCK]"
+#set -e SSH_AUTH_SOCK; set -e SSH_AGENT_PID; echo "AgentPID:[$SSH_AGENT_PID]; Socket[$SSH_AUTH_SOCK]"
+#ssh-agent-fish; ssh-env-echo
+#ssh-agent-fish -k; ssh-env-echo
+
+# Test switching two agents with different number of keys. Socket path (but not PID) defines which agent is used, keys are stored inside the agent. PID defines which agent is killed with `ssh-agent -k`
+#set -Ux prepid $SSH_AGENT_PID; set -Ux presock $SSH_AUTH_SOCK; echo [$prepid] [$presock]
+#set -Ux postpid $SSH_AGENT_PID; set -Ux postsock $SSH_AUTH_SOCK; echo [$postpid] [$postsock]
+#set -Ux SSH_AGENT_PID $prepid; set -Ux SSH_AUTH_SOCK $presock; echo [$SSH_AGENT_PID] [$SSH_AUTH_SOCK]
+#set -Ux SSH_AGENT_PID $postpid; set -Ux SSH_AUTH_SOCK $postsock; echo [$SSH_AGENT_PID] [$SSH_AUTH_SOCK]
+# mix it up
+#set -Ux SSH_AGENT_PID $prepid; set -Ux SSH_AUTH_SOCK $postsock; echo [$SSH_AGENT_PID] [$SSH_AUTH_SOCK]
+#set -e prepid; set -e postpid; set -e presock; set -e postsock; set -e SSH_AGENT_PID; set -e SSH_AUTH_SOCK
+#Maybe set the socket path manually? electricmonk.nl/log/2012/04/24/re-use-existing-ssh-agent-cygwin-et-al/
--- envirt.fish
+++ envirt.fish
@@ -1,12 +1,12 @@
 # Virtual Environment
 #Initialise Python, Ruby, Perl and Java environment if installed
-if which pyenv > /dev/null; status --is-interactive; and . (pyenv init -|psub); end
-if which pyenv-virtualenv-init > /dev/null; status --is-interactive; and . (pyenv virtualenv-init -|psub); end
-if which rbenv > /dev/null; status --is-interactive; and . (rbenv init -|psub); end
-if which nodenv > /dev/null; status --is-interactive; and . (nodenv init -|psub); end
-if which plenv > /dev/null; status --is-interactive; and . (plenv init -|psub); end
-#if which jenv > /dev/null; status --is-interactive; and . (jenv init -|psub); end	#not working
-if which jenv > /dev/null; fenv 'eval "$(jenv init -)"'; end
+if command -v pyenv > /dev/null; status --is-interactive; and . (pyenv init -|psub); end
+if command -v pyenv-virtualenv-init > /dev/null; status --is-interactive; and . (pyenv virtualenv-init -|psub); end
+if command -v rbenv > /dev/null; status --is-interactive; and . (rbenv init -|psub); end
+if command -v nodenv > /dev/null; status --is-interactive; and . (nodenv init -|psub); end
+if command -v plenv > /dev/null; status --is-interactive; and . (plenv init -|psub); end
+#if command -v jenv > /dev/null; status --is-interactive; and . (jenv init -|psub); end	#not working
+if command -v jenv > /dev/null; fenv 'eval "$(jenv init -)"'; end
 
 #$HOME/.nvm/versions/node/v5.0.0/bin
 #set -x WORKON_HOME=$HOME/.virtualenvs
