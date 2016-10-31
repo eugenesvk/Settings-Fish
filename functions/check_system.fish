578d1bc681c3b89df49923f7424ee5f4b71246b5
eugenesvk <eugenesvk@gmail.com>
2016-10-31 22:44:15 +0300

    Add Windows Subsystem for Linux separately as WinLinux

--- functions/check_system.fish
+++ functions/check_system.fish
@@ -1,6 +1,9 @@
 function check_system -d 'Display short system name (OSX, Linux, Win, NA)'
   set -l osname (uname | tr '[:upper:]' '[:lower:]' 2>/dev/null); or set -l osname (fish -c "uname | tr '[:upper:]' '[:lower:]'"); or set -l osname (/usr/bin/uname | tr '[:upper:]' '[:lower:]' 2>/dev/null)
   # fix for lacking MSYS2 path; 2nd needed for WSL as regular commands fails (bug https://github.com/Microsoft/BashOnWindows/issues/1210)
+  if test -e /mnt/c/Windows/System32/cmd.exe
+		set osname winlinux
+	end
   if test "$osname" = darwin
     echo OSX
   else if begin
@@ -11,6 +14,8 @@ function check_system -d 'Display short system name (OSX, Linux, Win, NA)'
     echo Win
   else if test "$osname" = linux
     echo Linux
+  else if test "$osname" = winlinux
+    echo WinLinux
   else
     echo NA
   end
