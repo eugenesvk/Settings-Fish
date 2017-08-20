9ff329a5d1d20bb3247afb49fec1fe8243049e72
eugenesvk <eugenesvk@users.noreply.github.com>
2017-08-20 21:23:08 +0300

    Replace . with source to avoid an error

--- envirt.fish
+++ envirt.fish
@@ -1,11 +1,11 @@
 # Virtual Environment
 #Initialise Python, Ruby, Perl and Java environment if installed
-if command -v pyenv > /dev/null; status --is-interactive; and . (pyenv init -|psub); end
-if command -v pyenv-virtualenv-init > /dev/null; status --is-interactive; and . (pyenv virtualenv-init -|psub); end
-if command -v rbenv > /dev/null; status --is-interactive; and . (rbenv init -|psub); end
-if command -v nodenv > /dev/null; status --is-interactive; and . (nodenv init -|psub); end
-if command -v plenv > /dev/null; status --is-interactive; and . (plenv init -|psub); end
-#if command -v jenv > /dev/null; status --is-interactive; and . (jenv init -|psub); end	#not working
+if command -v pyenv > /dev/null; status --is-interactive; and source (pyenv init -|psub); end
+if command -v pyenv-virtualenv-init > /dev/null; status --is-interactive; and source (pyenv virtualenv-init -|psub); end
+if command -v rbenv > /dev/null; status --is-interactive; and source (rbenv init -|psub); end
+if command -v nodenv > /dev/null; status --is-interactive; and source (nodenv init -|psub); end
+# if command -v plenv > /dev/null; status --is-interactive; and source (plenv init -|psub); end
+# if command -v jenv > /dev/null; status --is-interactive; and source (jenv init -|psub); end	#not working
 if command -v jenv > /dev/null; fenv 'eval "$(jenv init -)"'; end
 
 #$HOME/.nvm/versions/node/v5.0.0/bin
