05e98b8facc16962bb842b8239bc33611dbf9096
eugenesvk <eugenesvk@users.noreply.github.com>
2023-07-15 20:58:49 +0700

    add function: mkdir

---
+++ functions/md.fish
@@ -0,0 +1,13 @@
+#This will run the mkdir command, and if it is successful, change the current working directory to the one just created
+function md -w mkdir -d "Create a directory and CD to it"                #`-d` add description
+  command mkdir $argv
+  if test $status = 0
+    switch $argv[(count $argv)]
+      case '-*'
+
+      case '*'
+        cd $argv[(count $argv)]
+        return
+    end
+  end
+end
