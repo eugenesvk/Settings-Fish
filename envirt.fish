2f6dcd85d0f5fa6e28aa5d97fbc5f36fc9b7a14a
eugenesvk <eugenesvk@users.noreply.github.com>
2023-07-15 21:14:18 +0700

    update virtual env inits

--- envirt.fish
+++ envirt.fish
@@ -1,12 +1,16 @@
 # Virtual Environment
-#Initialise Python, Ruby, Perl and Java environment if installed
-if command -v pyenv > /dev/null; status --is-interactive; and source (pyenv init -|psub); end
-if command -v pyenv-virtualenv-init > /dev/null; status --is-interactive; and source (pyenv virtualenv-init -|psub); end
-if command -v rbenv > /dev/null; status --is-interactive; and source (rbenv init -|psub); end
-if command -v nodenv > /dev/null; status --is-interactive; and source (nodenv init -|psub); end
-# if command -v plenv > /dev/null; status --is-interactive; and source (plenv init -|psub); end
-# if command -v jenv > /dev/null; status --is-interactive; and source (jenv init -|psub); end	#not working
-if command -v jenv > /dev/null; fenv 'eval "$(jenv init -)"'; end
+# Initialise Python, Ruby, Perl and Java environments if installed
+if status -i
+  if command -q pyenv                      	; source (pyenv  init --path|psub); source (pyenv init -|psub)	; end
+  if command -q pyenv-virtualenv-init      	; source (pyenv  virtualenv-init -|psub)                      	; end
+  #if command -q "$HOME/.asdf/shims/direnv"	; eval (asdf exec direnv hook fish)                           	; end
+  if command -q rbenv                      	; source (rbenv  init -|psub)                                 	; end
+  if command -q nodenv                     	; source (nodenv init -|psub)                                 	; end
+  # if command -q plenv                    	; source (plenv  init -|psub)                                 	; end
+  # if command -q jenv                     	; source (jenv   init -|psub)                                 	; end
+  if command -q zoxide                     	; source (zoxide init fish|psub)                              	; end
+end
+
 
 #$HOME/.nvm/versions/node/v5.0.0/bin
 #set -x WORKON_HOME=$HOME/.virtualenvs
